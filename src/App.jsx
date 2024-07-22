755fca493c2526dee4d3ad5cdcaac2877dcb1e96
Vaibhav Singh <vaibhavraghuvanshi93@gmail.com>
2024-07-22 16:54:19 +0530

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./app.scss";
 import Contact from "./components/Contact/Contact";
 import Cursor from "./components/Cursor/Cursor";
-import Hero from "./components/hero/Hero";
+import Hero from "./components/Hero/Hero";
 import NavBar from "./components/Navbar/Navbar"
-import Parallax from "./components/parallax/Parallax";
-import Portfolio from "./components/portfolio/Portfolio";
-import Services from "./components/services/Services";
+import Parallax from "./components/Parallax/Parallax";
+import Portfolio from "./components/Portfolio/Portfolio";
+import Services from "./components/Services/Services";
 
 const App = () => {
   return (
