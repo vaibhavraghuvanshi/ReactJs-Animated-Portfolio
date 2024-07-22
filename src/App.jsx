029330901ab4417c632247802f52d73b44a536cc
Vaibhav Singh <vaibhavraghuvanshi93@gmail.com>
2024-07-22 16:44:40 +0530

    Code Ajdustments

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import Test from "./Test";
 import "./app.scss";
 import Contact from "./components/contact/Contact";
-import Cursor from "./components/cursor/Cursor";
+import Cursor from "./components/Cursor/Cursor";
 import Hero from "./components/hero/Hero";
-import Navbar from "./components/navbar/Navbar";
+import NavBar from "./components/Navbar/Navbar"
 import Parallax from "./components/parallax/Parallax";
 import Portfolio from "./components/portfolio/Portfolio";
 import Services from "./components/services/Services";
@@ -13,7 +13,7 @@ const App = () => {
     <div>
       <Cursor />
       <section id="Homepage">
-        <Navbar />
+        <NavBar />
         <Hero />
       </section>
       <section id="Services">
